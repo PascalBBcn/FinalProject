de9ccba0e788c28a4f1de7cc64485722fc9ae839
PascalBBcn <pascalbbcn@gmail.com>
2025-08-09 11:25:13 +0700

    Updated door tiles

--- Assets/Scripts/DungeonGeneration/BSPDungeonGenerator.cs
+++ Assets/Scripts/DungeonGeneration/BSPDungeonGenerator.cs
@@ -10,7 +10,7 @@ public class BSPDungeonGenerator : MonoBehaviour
     [SerializeField] private int dungeonHeight = 80;
     [SerializeField] private int minRoomWidth = 13;
     [SerializeField] private int minRoomHeight = 13;
-    private int padding = 2; // For padding between rooms
+    private int padding = 1; // For padding between rooms
     [SerializeField] private int numberOfDigs = 200;
 
 
